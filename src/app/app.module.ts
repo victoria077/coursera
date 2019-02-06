08566558392008ab6930134396a6a4581d600569
scary <imbamrgrey@gmail.com>
2019-02-06 16:05:17 +0100

    Reactive Forms Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -52,9 +55,12 @@ import { FormsModule } from '@angular/forms';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     AppRoutingModule,
     MatDialogModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   entryComponents: [LoginComponent],
   providers: [DishService, PromotionService, LeaderService],
