2d8b0d712e8576404d31f599b76b36928fa2f9dd
scary <imbamrgrey@gmail.com>
2019-02-24 11:17:40 +0100

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -8,6 +8,7 @@ import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Rating } from '../shared/rating';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 const DISH = {
     id: '0',
@@ -55,7 +56,20 @@ const DISH = {
 @Component({
     selector: 'app-dishdetail',
     templateUrl: './dishdetail.component.html',
-    styleUrls: ['./dishdetail.component.scss']
+    styleUrls: ['./dishdetail.component.scss'],
+    animations: [
+        trigger('visibility', [
+            state('shown', style({
+                transform: 'scale(1.0)',
+                opacity: 1
+            })),
+            state('hidden', style({
+                transform: 'scale(0.5)',
+                opacity: 0
+            })),
+            transition('* => *', animate('0.5s ease-in-out'))
+        ])
+    ]
 })
 export class DishdetailComponent implements OnInit {
     @ViewChild('fform') feedbackFormDirective;
@@ -64,7 +78,7 @@ export class DishdetailComponent implements OnInit {
     dishIds: string[];
     prev: string;
     next: string;
-
+    visibility = 'shown';
 
     feedbackForm: FormGroup;
     rating: Rating;
@@ -153,8 +167,8 @@ export class DishdetailComponent implements OnInit {
     ngOnInit() {
         this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
         this.route.params
-            .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-            .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+        .pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); }))
+            .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
                 errmess => this.errMess = <any>errmess);
     }
 
