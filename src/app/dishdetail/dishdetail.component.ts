3774eb27f3a7b89bcccafae467dec039188b7a9b
scary <imbamrgrey@gmail.com>
2019-02-24 11:32:44 +0100

    Animations Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -8,8 +8,7 @@ import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Rating } from '../shared/rating';
-import { trigger, state, style, animate, transition } from '@angular/animations';
-
+import { visibility, flyInOut, expand } from '../animations/app.animation';
 const DISH = {
     id: '0',
     name: 'Uthappizza',
@@ -57,18 +56,15 @@ const DISH = {
     selector: 'app-dishdetail',
     templateUrl: './dishdetail.component.html',
     styleUrls: ['./dishdetail.component.scss'],
+    // tslint:disable-next-line:use-host-property-decorator
+    host: {
+        '[@flyInOut]': 'true',
+        'style': 'display: block;'
+        },
     animations: [
-        trigger('visibility', [
-            state('shown', style({
-                transform: 'scale(1.0)',
-                opacity: 1
-            })),
-            state('hidden', style({
-                transform: 'scale(0.5)',
-                opacity: 0
-            })),
-            transition('* => *', animate('0.5s ease-in-out'))
-        ])
+        visibility(),
+        flyInOut(),
+        expand()
     ]
 })
 export class DishdetailComponent implements OnInit {
